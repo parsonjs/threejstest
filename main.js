efbc64d827ead338ec7403b8461574ff43b63490
parsonjs@amazon.com <parsonjs@amazon.com>
2023-06-12 10:42:00 -0700

    added pause button

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
+//scene setup
 const backgroundColor = 0x000000;
 
 
@@ -33,12 +34,11 @@ window.addEventListener( 'resize', function () {
   renderer.setSize( window.innerWidth, window.innerHeight );
 }, false );
 
+
 document.body.appendChild( renderer.domElement);
 
 function renderScene(){ renderer.render( scene, camera ); }
 renderCalls.push(renderScene);
-
-
 
 
 var light = new THREE.PointLight( 0xffffcc, 20, 200 );
@@ -50,21 +50,35 @@ light2.position.set( 30, -10, 30 );
 scene.add( light2 );
 
 
-
 var mixer
 const loader = new GLTFLoader();
 loader.crossOrigin = true;
+var pauseButton = document.getElementById('pauseButton');
+
+
+/*loads in gltf animations and handles pausing should probably 
+split this into multiple functions
+*/
 loader.load( 'bee_gltf.gltf', function ( gltf ) {
     var object = gltf.scene;
     object.position.set(0, 0, 0);
+    var isAnimationPaused = false
     mixer = new THREE.AnimationMixer(gltf.scene);
     gltf.animations.forEach((clip)=> {
-      mixer.clipAction(clip).play();
-      
+        mixer.clipAction(clip).play();
+        pauseButton.onclick = function StartAnimation() {
+          if(!isAnimationPaused){
+            isAnimationPaused = true;
+          }else{
+            isAnimationPaused = false;
+          }
+          mixer.clipAction(clip).paused = isAnimationPaused
+      }
     })
     scene.add( object );
 });
 
+//Continuous function to handle the playback clock
 var clock = new THREE.Clock();
 function animate(){
   requestAnimationFrame(animate);
